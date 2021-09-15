4f0f773808f696475f5d9e55a61db9fd047fe88c
Antonio Mourão <antonio.sobrinho@caffeex.com>
2021-09-15 17:47:40 -0300

    change database to postgresql

--- post_statistics/post_statistics/settings.py
+++ post_statistics/post_statistics/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'post_statistics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':
+        'django.db.backends.postgresql_psycopg2',
+        'NAME':
+        'post_statistics',
+        'USER':
+        'postgres',
+        'PASSWORD':
+        'postgres',
+        'HOST':
+        'localhost',
     }
 }
 
